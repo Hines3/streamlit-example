a1d91d779900e3e14c5050db72cbb8702676ae86
Hines3 <115641462+Hines3@users.noreply.github.com>
2022-12-18 14:57:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,44 +1,12 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
-
-
-
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 import yfinance as yf
 
+# Get the stock ticker and date range from the user
+ticker = st.text_input("Enter a stock ticker (e.g. AAPL):")
+date_range = st.date_range_input("Enter a date range:")
 
-start_date = "2015-01-01"
-stock_list = ['USO', '^OVX', 'SPY', '^TNX', 'DX-Y.NYB']
+# Use the Yahoo Finance API to retrieve the stock data
+data = yf.download(ticker, start=date_range[0], end=date_range[1])
 
-
-def getClose(tickers, start_date):
-
-  df_final = pd.DataFrame()
-
-  for stock in tickers:
-    df = yf.download(stock, start=start_date).reset_index()
-    df = df.fillna(0)
-    close = df['Close']
-    
-    df_final[stock] = close
-
-  return df_final
-
-def findCorr(table):
-  
-  corr = table.corr()
-  fig, ax = plt.subplots()
-  fig.set_size_inches(11,11)
-
-  return sns.heatmap(corr)
-
-test = getClose(stock_list, start_date)
-final = findCorr(test)
-print(final)
+# Plot the stock chart using Streamlit's line_chart function
+st.line_chart(data["Close"])
