1e6f45d8ccf12a13f10a4011bc0be67d0706d645
Hines3 <115641462+Hines3@users.noreply.github.com>
2022-12-18 15:01:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import yfinance as yf
 
 # Get the stock ticker and date range from the user
 ticker = st.text_input("Enter a stock ticker (e.g. AAPL):")
-date_range = st.date_range_input("Enter a date range:")
+date_range = st.date_input("Enter a date range:")
 
 # Use the Yahoo Finance API to retrieve the stock data
 data = yf.download(ticker, start=date_range[0], end=date_range[1])
