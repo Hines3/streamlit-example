d3efae3758c77d6273f7ce922486655c04d6ee67
Hines3 <115641462+Hines3@users.noreply.github.com>
2022-12-18 15:02:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,11 @@ import yfinance as yf
 
 # Get the stock ticker and date range from the user
 ticker = st.text_input("Enter a stock ticker (e.g. AAPL):")
-date_range = st.date_input("Enter a date range:")
+date_range = st.date_input("Enter a start date:")
+date_range2 = st.date_input("Enter a end date:")
 
 # Use the Yahoo Finance API to retrieve the stock data
-data = yf.download(ticker, start=date_range[0], end=date_range[1])
+data = yf.download(ticker, start=date_range, end=date_range2)
 
 # Plot the stock chart using Streamlit's line_chart function
 st.line_chart(data["Close"])
